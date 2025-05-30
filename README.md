4ad76c15d72dd32730cfc2970952de120b4e24e4
chLi <565353780@qq.com>
2025-05-30 18:30:51 +0800

    add source code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# diff-curvature
+# Diff Curvature
+
+## Source
+
+```bash
+https://github.com/Luo-Yihao/DiffCurvature
+```
+
+## Setup
+
+```bash
+conda create -n dc python=3.10
+conda activate dc
+./setup.sh
+```
+
+## Run
+
+```bash
+python demo.py
+```
+
+## Enjoy it~
+
---
+++ setup.sh
@@ -0,0 +1,14 @@
+cd ..
+git clone https://github.com/NVlabs/nvdiffrast
+
+pip install -U torch torchvision torchaudio
+
+pip install -U trimesh matplotlib opencv-python einops
+
+pip install "git+https://github.com/facebookresearch/pytorch3d.git"
+
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.7.0+cu128.html
+
+cd nvdiffrast
+
+./run_sample.sh ./samples/torch/cube.py --resolution 32
