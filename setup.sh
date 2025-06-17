fdbb1aab7f6716b06e9998a104ef9a6f0775b2cb
chLi <565353780@qq.com>
2025-06-17 21:25:19 +0800

    clone pytorch3d for stable setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 cd ..
 git clone https://github.com/NVlabs/nvdiffrast
+git clone https://github.com/facebookresearch/pytorch3d.git
 
 pip install -U torch torchvision torchaudio
 
 pip install -U trimesh matplotlib opencv-python einops
 
-pip install "git+https://github.com/facebookresearch/pytorch3d.git"
+# pip install "git+https://github.com/facebookresearch/pytorch3d.git"
 
 pip install torch-scatter -f https://data.pyg.org/whl/torch-2.7.0+cu128.html
 
 pip install pyglet==1.5.13
 
 cd nvdiffrast
+pip install .
+
+cd ../pytorch3d
 pip install .
